1939225bcc35a790080450fc5506974f09052456
Jitesh Doshi <jitesh@doshiland.com>
2012-08-25 12:34:52 -0400

    Added popular tag show/hide and limit capability.

--- popular_tags.js
+++ popular_tags.js
@@ -2,7 +2,7 @@
 
 Drupal.behaviors.popular_tags = {
   attach: function(context, settings) {
-    $('.tag-terms .term', context).click(function(event) {
+    $('.popular-tags .tag-terms .term', context).click(function(event) {
       $this = $(this);
       var inp = $this.parents('.form-item').find('input:text.form-autocomplete');
       var val = inp.val();
@@ -20,34 +20,25 @@ Drupal.behaviors.popular_tags = {
       }
       return false;
     });
-    if(Drupal.settings.popular_tags && Drupal.settings.popular_tags.popular_tids) {
-      for(field_name in Drupal.settings.popular_tags.popular_tids) {
+    if(Drupal.settings.popular_tags) {
+      for(field_name in Drupal.settings.popular_tags) {
         var _field_name = field_name.replace('_', '-');
-        var element_id = 'edit-' + _field_name + '-und';
-        var container = $('#' + element_id, context);
-        var tids = Drupal.settings.popular_tags.popular_tids[field_name];
-        for(t in tids) {
-          var tid = tids[t];
-          container.find('.form-item-' + _field_name + '-und-' + tid).addClass('popular-term');
-        }
-        // hide all terms
-        var show = $('<a href="#">Show All</a>');
-        var hide = $('<a href="#">Show Popular</a>');
+        var field_container = $('.form-item-' + _field_name + '-und', context);
+        var tag_container = field_container.find('.popular-tags');
+        var show = tag_container.find('a.show-all-terms');
+        var hide = tag_container.find('a.show-popular-terms');
         show.click(function() {
           $(this).hide();
           hide.show();
-          container.find('.form-item').show();
+          tag_container.find('.term').not('.popular').show();
           return false;
         });
         hide.click(function() {
           $(this).hide();
           show.show();
-          container.find('.form-item').hide();
-          container.find('.form-item.popular-term').show();
+          tag_container.find('.term').not('.popular').hide();
           return false;
         });
-        container.after(show);
-        container.after(hide);
         hide.click();
       }
     }
